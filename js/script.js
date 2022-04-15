650e21a316d29b4b8a155a81850c023039e8cbf2
Vladimir <exemple@gmail.com>
2022-04-16 00:33:22 +0300

    movie script

--- js/script.js
+++ js/script.js
@@ -1,2 +1,17 @@
 "use strict";
 console.log('1');
+
+let numberOfFilms = prompt("how much film you look", "");
+let personalMovieDB = {
+    count : numberOfFilms,
+    movies : {},
+    actors : {},
+    genres : [],
+    privat : false
+};
+let lastFilm = prompt('What is last film yo watch', '');
+let ratingFilm = prompt('Give your reting for this film', '');
+personalMovieDB.movies[lastFilm] = ratingFilm;
+let lastFilm1 = prompt('What is last film yo watch', '');
+let ratingFilm1 = prompt('Give your reting for this film', '');
+personalMovieDB.movies[lastFilm1] = ratingFilm1;
